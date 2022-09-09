20c6de43e364c07d6083683bc373923615c508c8
TranTam <tamvaa1306@gmail.com>
2022-09-10 00:50:44 +0700

    types and clear function

--- src/App.vue
+++ src/App.vue
@@ -7,24 +7,10 @@
 
 <script lang="ts">
 import AppHeader from "@/components/AppHeader.vue";
-import { ref } from "@vue/reactivity";
-
-// types
-import NumberSystem from "./types/NumberSystem";
 
 export default {
   components: {
     AppHeader,
-  },
-  setup() {
-    const numberSystems = ref<NumberSystem[]>([
-      { title: "Binary", value: "binary", id: "1" },
-      { title: "Decimal", value: "decimal", id: "2" },
-      { title: "Octal", value: "octal", id: "3" },
-      { title: "Hexadecimal", value: "hexadecimal", id: "4" },
-    ]);
-
-    return { numberSystems };
   },
 };
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,10 @@
     <select name="" id=""></select>
     <div class="buttons">
       <button class="convert">Convert</button>
-      <button class="clear">Clear</button>
+      <button class="clear" v-on:click="clearInput('')">Clear</button>
+    </div>
+    <div class="output">
+      <h1>{{ outputNumber }}</h1>
     </div>
   </div>
 </template>
@@ -12,13 +15,28 @@
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 
+// types
+import NumberSystem from "../types/NumberSystem";
+
 export default defineComponent({
   name: "HomeView",
 
   setup() {
-    const inputNumber = ref("");
+    const inputNumber = ref<string | number>("");
+    const outputNumber = ref<string | number>("");
 
-    return { inputNumber };
+    const numberSystems = ref<NumberSystem[]>([
+      { title: "Binary", value: "binary", id: "1" },
+      { title: "Decimal", value: "decimal", id: "2" },
+      { title: "Octal", value: "octal", id: "3" },
+      { title: "Hexadecimal", value: "hexadecimal", id: "4" },
+    ]);
+
+    const clearInput = (input: string) => {
+      inputNumber.value = input;
+    };
+
+    return { inputNumber, numberSystems, outputNumber, clearInput };
   },
 });
 </script>
