42c25204d5a9b57fbdde7c793b633f799789c149
TranTam <tamvaa1306@gmail.com>
2022-09-10 12:25:19 +0700

    convert method

---
+++ src/types/ConvertMethod.ts
@@ -0,0 +1,15 @@
+type ConvertMethod =
+  | "decToBin"
+  | "decToOct"
+  | "decToHex"
+  | "binToDec"
+  | "binToOct"
+  | "binToHex"
+  | "octToBin"
+  | "octToDec"
+  | "octToHex"
+  | "hexToBin"
+  | "hexToOct"
+  | "hexToDec";
+
+export default ConvertMethod;
